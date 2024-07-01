91dc2fee93a2cca1b8c0a0b134840d90ca60ddc1
Daisha <daisha05311994@gmail.com>
2024-07-01 09:48:38 -0400

    FSDIcommit2

---
+++ setup.sql
@@ -0,0 +1,21 @@
+INSERT INTO task (
+    name,
+    summary,
+    description
+) VALUES
+(
+
+    "Wash car",
+    "The car needs to be washed",
+    "Make sure the car gets waxed after it is washed"
+),
+(
+    "Walk the dog",
+    "Fido needs their exercise",
+    "Make sure to do at least 2 laps around the park"
+),
+(
+    "Buy groceries",
+    "Go to the supermarket and buy groceries",
+    "We need: Eggs, Milk, Cereal and Coffee."
+);
